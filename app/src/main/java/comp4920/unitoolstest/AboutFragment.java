5396de75bcfaaa01563bec1d06ff4bf65ae99a41
grantweii <z5078316@ad.unsw.edu.au>
2018-09-14 16:58:43 +1000

    Tabbed Activity partially done

---
+++ app/src/main/java/comp4920/unitoolstest/AboutFragment.java
@@ -0,0 +1,23 @@
+package comp4920.unitoolstest;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class AboutFragment extends Fragment {
+
+    private static final String TAG = "AboutFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_about_fragment,container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/comp4920/unitoolstest/MainActivity.java
@@ -0,0 +1,19 @@
+package comp4920.unitoolstest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(getApplicationContext(), SocietyActivity.class);
+        startActivity(intent);
+
+    }
+}
